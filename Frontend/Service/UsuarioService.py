e7f88dfa411d60477ecea33148a96dfd4af66600
Omarcito <balderascabralomar@gamil.com>
2026-04-23 16:47:36 -0600

    Correccion en parametros y error de post

--- Frontend/Service/UsuarioService.py
+++ Frontend/Service/UsuarioService.py
@@ -3,15 +3,22 @@ import requests
 BASE_URL="Link de render"
 
 def post(endpoint, data):
-    return requests.post(f"{BASE_URL}{endpoint}",json=data).json()
+    try:
+        response = requests.post(f"{BASE_URL}{endpoint}",json=data)
+        return response.json()
+    except Exception as e:
+        return {"Success": False, "error": str(e)}
+
 def login(email, password):
     return post("/auth/login",{
         "email":email,
         "password": password
     })
-def register(nombre,email,password):
-    return post("auth/register",{
+def register(nombre,email,password,pregunta,respuesta):
+    return post("/auth/register",{
         "nombre":nombre,
         "email":email,
-        "password":password
+        "password":password,
+        "pregunta_recuperacion":pregunta,
+        "respuesta_recuperacion":respuesta
     })
